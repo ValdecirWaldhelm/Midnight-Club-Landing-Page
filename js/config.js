5add89435ffe436dce978f6d99c1dd8e0df6a5ee
Valdecir Waldhelm Morgado <valdwald81@gmail.com>
2020-11-24 01:26:12 -0300

    mais funcionalidades
    Funcionalidades em javascript na tag video com buttins entre outros,como background parallax,formulario method post

--- js/config.js
+++ js/config.js
@@ -2,12 +2,31 @@
 var video = document.getElementById("myVideo");
 var btn = document.getElementById("myBtn");
 
-function myFunction() {
+var sound = document.getElementById("myVideo");
+var btnsound = document.getElementById("soundBtn");
+
+function myFunctionvideo() {
   if (video.paused) {
     video.play();
     btn.innerHTML = "Pause";
-  } else {
+  } else{
     video.pause();
     btn.innerHTML = "Play";
   }
 }
+
+function enableMute(){
+  video.muted = true;
+}
+
+function disableMute(){
+  video.muted = false;
+}
+
+
+myFunction()
+
+enableMute()
+
+disableMute()
+
